174de8773b5098f90a8bc82c9995681fac75d369
Wisdom Mpamugo <wisdommpam99@gmail.com>
2025-10-13 20:37:41 +0100

    hooks

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,11 +1,21 @@
 "use client";
 
 import Navbar from "@/components/general/navbar";
+import { Button } from "@/components/ui/button";
+import { useBoards } from "@/lib/hooks/useBoards";
 import { useUser } from "@clerk/nextjs";
+import { Plus } from "lucide-react";
 import React from "react";
 
 const Dashboard = () => {
   const { user } = useUser();
+
+  const {createBoard} = useBoards()
+
+  const handleCreateBoard = async () => {
+    await createBoard()
+  }
+
   return (
     <div className="min-h-screen bg-gray-50">
       <Navbar />
@@ -20,6 +30,10 @@ const Dashboard = () => {
           <p className="text-gray-600">
             Here&apos;s what happening with your boards today
           </p>
+          <Button className="w-full sm:w-auto" onClick={handleCreateBoard}>
+            <Plus className="size-4 mr-2" />
+            Create Board
+          </Button>
         </div>
       </main>
     </div>
---
+++ lib/hooks/useBoards.ts
@@ -0,0 +1,11 @@
+export function useBoards() {
+  async function createBoard(boardData: {
+    title: string;
+    description?: string;
+    color?: string;
+  }) {
+    
+  }
+
+  return { createBoard };
+}
